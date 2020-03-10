da33317b6e43069d1ddfd06bb5f0d2867dea914c
Sam Gorick <samgorick.sg@gmail.com>
2020-03-10 22:48:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,7 +18,8 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
+  
+  #Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
 
